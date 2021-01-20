0125b55223f99603495b5b852bd752a6668f6b9b
artegoser <59178854+artegoser@users.noreply.github.com>
2021-01-20 19:49:13 +0300

    bind, recvfrom

--- SUDP/SUDP.py
+++ SUDP/SUDP.py
@@ -1,3 +1,16 @@
+import socket
 class sUDPsocket():
-    def __init__():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    def bind(self, addres):
+        self.sock.bind(addres)
+    def recvfrom(self, bytes):
+        msg , address = self.sock.recvfrom(bytes)
+        if msg.decode("utf-8") == "ok":
+            return
+        self.sock.sendto(("ok").encode("utf-8"),address)
+        return msg, address
+    #def recv(self, bytes):
+    #    pass
+    def sendto(self, msg):
         pass
